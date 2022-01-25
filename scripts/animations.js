8ca7f2c7369865f66270b88e3c12c5e23a58dce6
isaac <64103675+weytools@users.noreply.github.com>
2022-01-25 12:03:12 -0500

    intro anim work

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,8 +1,8 @@
 export function introAnimation(){
     console.log('intro animation')
-    anime({
-        targets: '#topNav',
-        opacity: 0,
-        delay: 1000
-    })
+    // anime({
+    //     targets: '#topNav',
+    //     opacity: 0,
+    //     delay: 1000
+    // })
 }
