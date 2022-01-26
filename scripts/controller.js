7d895cbe95e3f6d315f7315da131c969ad41f5b8
isaac <64103675+weytools@users.noreply.github.com>
2022-01-25 21:47:42 -0500

    Scroll to top fix

--- scripts/controller.js
+++ scripts/controller.js
@@ -3,6 +3,7 @@ import getProjects from "./projects.js";
 import * as ani from "./animations.js";
 
 document.addEventListener("DOMContentLoaded", function(){
+    history.scrollRestoration = 'manual';
     window.scroll(0,0);
     ani.setupListeners();
     ani.introAnimation();
